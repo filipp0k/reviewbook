5ed7e6e54dbc851b03d68372dab8b5da1d725128
Филипп Дюдин <filipp.dyudin@gmail.com>
2017-12-15 05:06:50 +0300

    Created Detailed view Added Igor pics You still won't read it

--- Reviewbook/ViewController.swift
+++ Reviewbook/ViewController.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var Name: UITextField!
+    @IBOutlet weak var TextView: UITextView!
+    @IBOutlet weak var Image: UIImageView!
+    
+    @IBAction func tapImage(_ sender: UITapGestureRecognizer) {
+        print("Does it work?")
+        view.endEditing(true)
+        //Name.resignFirstResponder()
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
+    
+    //ГЫ
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Name.delegate = self
+        
     }
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        Image.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    func textFieldShouldReturn(_ textfield: UITextField) -> Bool {
+        textfield.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        Label.text = textField.text
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
